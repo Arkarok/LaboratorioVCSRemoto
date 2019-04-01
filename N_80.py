88caeb8b5fa67b72717f75c0da26d5cce7e6a247
ARKAROK <henry.nunez.2019@upb.edu.co>
2019-04-01 18:51:28 -0500

    Ya termine por fin dijo el artas v:

--- N_80.py
+++ N_80.py
@@ -1,16 +1,18 @@
 print("Algoritmo que imprima el siguiente patron")
 
-for num in range(1,5+1):
-	if num==1:
-		print(1)
-	elif num==2:
-		print(2,3)
-	elif num==3:
-		print(4,5,6)
-	elif num==4:
-		print(7,8,9,10)
-	else:
-		print(11,12,13,14,15)
+n=0
+
+a=int(input("Digita el numero de filas: "))
+
+for i in range(a+1):
+	for e in range(0,i):
+		b=i+e
+		print(end=str(b))
+
+	print(" ")
+
+	i=i+n
+	a+=1	
 
 print("Final del programa")	
 
